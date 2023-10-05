b7dce2a65d63ece0ad9ba43197bb3ebaa3321098
Rebot <96078724+reboot-dev-bot@users.noreply.github.com>
2023-10-05 10:00:39 +0000

    [Release] Update the examples

--- hello-world/backend/src/main.py
+++ hello-world/backend/src/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from greeter_servicer import GreeterServicer
+from hello_world.v1.greeter_rsm import Greeter, GreetResponse
 from resemble.aio.applications import Application
 from resemble.aio.workflows import Workflow
 
